82944d3118e63a0686948cf91783a23f1a67bf77
luisyuburi <luisyuburi@outlook.com>
2024-01-08 08:48:34 -0500

    header styles features

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -1,16 +1,37 @@
-import {Link} from '@remix-run/react'
+import { Link, useLocation } from '@remix-run/react'
+import logo from '../../public/img/logo.svg'
 const Header = () => {
+  const location = useLocation()
   return (
     <header className="header">
       <div className="contenedor barra">
-        <div className="logo">
-
-        </div>
+        <Link to="/" className="logo">
+          <img className='logo' src={logo} alt="logo image" />
+        </Link>
         <nav className="navegacion">
           <Link
-          to="/"
+            to="/"
+            prefetch='render'
           >
             Home
+          </Link>
+          <Link
+            to="/shop"
+            prefetch='render'
+          >
+            Shop
+          </Link>
+          <Link
+            to="/blog"
+            prefetch='render'
+          >
+            Blog
+          </Link>
+          <Link
+            to="/about"
+            prefetch='render'
+          >
+            About
           </Link>
         </nav>
 
--- app/routes/about/index.tsx
+++ app/routes/about/index.tsx
@@ -1,3 +1,19 @@
+import type { LinksFunction, MetaFunction } from "@remix-run/react/dist/routeModules";
+
+// export const links: LinksFunction = () => [
+//   {
+//     rel: 'stylesheet',
+//     href: 'header.css'
+//   }
+// ]
+
+export const meta: MetaFunction = () => {
+  return [
+    { title: "About | AguitarLA" },
+    { name: "description", content: "Welcome to Remix!" },
+  ];
+};
+
 export default function Index() {
   return (
     <h1>About Us</h1>
